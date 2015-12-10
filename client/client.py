8b9969f827909c89116d9ce75d35b9f74f170b21
jmiraoui <jmiraoui@ipb.fr>
2015-12-10 17:32:43 +0100

    ajout deplacement dans le client

--- client/client.py
+++ client/client.py
@@ -2,52 +2,60 @@ from functions import *
 import json
 import requests
 import linecache
+import time
+import random
 
+
+defaultPort = 8080
 
 def postPosition(url, car):
 	payload = json.loads(get_payload(car))
 	headers = {'content-type': 'application/json'}
-	s=requests.Session()
-	r = None
 	try:
-		r = s.post(url, data = json.dumps(payload), headers = headers)
+		r = requests.post(url, data = json.dumps(payload), headers = headers, allow_redirects=False)
+                if r.status_code == 303:
+                        print (r.text)
+                        newUrl = 'http://'+r.headers['location']+':'+str(defaultPort)
+                        #postPosition(newUrl, car)
+                else:
+                        print(r.text)
 	except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-		print ("Failed to connect to "+url)
 		print e
-	return r
 
+
+def straight_move(url, car, dest_y, timeToPost):
+        for i in range(car.y,dest_y): 
+              car.move(car.x, i, timeToPost)
+              print (str(car.x)+' '+str(car.y))
+              postPosition(url, car)
+              time.sleep(timeToPost)
+              
+        
+def random_move (url, car, cpt, timeToPost):
+        while cpt != 0:
+                dest_x = random.randrange(20)
+                dest_y = random.randrange(20)
+                print ('------------- FROM '+str(car.x)+' '+str(car.y)+' Going to '+str(dest_x)+' '+str(dest_y)+'---------------')
+
+                while car.arrived is False:
+                        car.move(dest_x, dest_y, timeToPost)
+                        print (str(car.x)+' '+str(car.y))
+                        postPosition(url, car)
+                        time.sleep(timeToPost)
+
+                print (str(dest_x)+' '+str(dest_y)+' <------------------- REACHED\n')
+                car.arrived = False
+                cpt -= 1
 
 #Main
 defaultServer = linecache.getline('./client.conf', 1).strip()
 backupServer = linecache.getline('./client.conf', 2).strip()
-defaultPort = '8080'
 
 serverToContact = defaultServer
-c=Car(1,1,get_mac())
+c=Car(1,10,get_mac(),2.5)
 
-#Premiere connection determinant l'utilisation du serveur de secours
-res = postPosition(serverToContact, c)
-if res is None:
-		serverToContact = backupServer
-		print('BACKUP SERVER '+serverToContact)
-		res = postPosition(serverToContact, c)
-
-#"Deplacement de la voiture"
-for i in range(5,15) :
-	c.refresh(1, i)
-	res = postPosition(serverToContact, c)
-
-	if res is None:
-		break
-
-	print ('pour i = '+str(i))
-	if "OK" in res.text: 
-    		print('OK')
-	elif "Not Found" in res.text:
-    		print('Not Found')
-	else:
-    		serverToContact = 'http://'+res.text.replace("Cannot GET /","").strip()+':'+defaultPort+'/'
-		print ('New server to contact: '+serverToContact)
+#straight_move(serverToContact, c, 20, 1)
+random_move(serverToContact, c, 2, 1)
 
 
 print ('fin du programme')
--- client/functions.py
+++ client/functions.py
@@ -3,16 +3,42 @@ from math import *
 from point import *
 
 class Car(Point):
-    def __init__(self,x,y,id):
+
+    def __init__(self,x,y,id, v):
         Point.__init__(self,x,y)
         self.id=id
+        self.v=v
+        self.arrived=False
 
-    def affichage(self):
+    def print_position(self):
         return '('+str(self.x)+';'+str(self.y)+';'+str(self.id)+')'
 
-    def refresh(self,x,y):
+    def refresh_position(self,x,y):
         self.x=x
 	self.y=y
+
+    def move(self, destX, destY, time):
+        distX = destX-self.x
+        distY = destY-self.y
+        
+        if distX > self.v*time:
+            self.x += self.v*time
+        elif abs(distX) <= self.v*time:
+            self.x = destX
+        else:
+            self.x -= self.v*time
+
+
+        if distY > self.v*time:
+            self.y += self.v*time
+        elif abs(distY) <= self.v*time:
+            self.y = destY
+        else:
+            self.y -= self.v*time
+        
+        if self.x == destX and self.y == destY:
+            self.arrived = True
+        
 
 
 def print_mac():
@@ -24,7 +50,6 @@ def payloadtest():
 
 def get_payload(car):
     return'{ \"client\":{\"id\":'+str(car.id)+',\"position\":{\"lat\":'+str(car.x)+',\"lon\":'+str(car.y)+'}}}' 
-
 
 
 
